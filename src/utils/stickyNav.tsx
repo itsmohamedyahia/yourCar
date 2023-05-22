9d27302d95d4438cef02abf4068642deb06ba784
itsmohamedyahia <justmyahia@gmail.com>
2023-05-22 21:10:21 +0300

    make sticky nav work again and some refactor

--- src/utils/stickyNav.tsx
+++ src/utils/stickyNav.tsx
@@ -1,21 +1,22 @@
-// let sectionHeroEl =
-//   document.querySelector(".section--hero") || document.createElement("div");
-// let observer = new IntersectionObserver(
-//   function (entries) {
-//     const intersectingState = entries[0].isIntersecting;
-//     if (!intersectingState) {
-//       console.log(entries);
-//       document.body.classList.add("sticky");
-//     }
-//     if (intersectingState) {
-//       document.body.classList.remove("sticky");
-//     }
-//   },
-//   {
-//     root: null,
-//     threshold: 0,
-//     rootMargin: "-80px"
-//   }
-// );
+let sectionHeroEl =
+  document.querySelector(".section--hero") || document.createElement("div");
+let observer = new IntersectionObserver(
+  function () {
+    // entries should be added to function parameter
+    // const intersectingState = entries[0].isIntersecting;
+    // if (!intersectingState) {
+    //   console.log(entries);
+    //   document.body.classList.add("sticky");
+    // }
+    // if (intersectingState) {
+    //   document.body.classList.remove("sticky");
+    // }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px"
+  }
+);
 
-// observer.observe(sectionHeroEl);
+observer.observe(sectionHeroEl);
