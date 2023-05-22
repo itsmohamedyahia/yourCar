fdba8467e16963811c7f02b0aa3bb2d31e87aabf
itsmohamedyahia <justmyahia@gmail.com>
2023-05-22 21:11:23 +0300

    delete comments in stickyNav

--- src/utils/stickyNav.tsx
+++ src/utils/stickyNav.tsx
@@ -1,22 +1,9 @@
 let sectionHeroEl =
   document.querySelector(".section--hero") || document.createElement("div");
-let observer = new IntersectionObserver(
-  function () {
-    // entries should be added to function parameter
-    // const intersectingState = entries[0].isIntersecting;
-    // if (!intersectingState) {
-    //   console.log(entries);
-    //   document.body.classList.add("sticky");
-    // }
-    // if (intersectingState) {
-    //   document.body.classList.remove("sticky");
-    // }
-  },
-  {
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px"
-  }
-);
+let observer = new IntersectionObserver(function () {}, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-80px"
+});
 
 observer.observe(sectionHeroEl);
